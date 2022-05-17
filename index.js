4cde5f4992aa8eafc38b251a144febfba16c9b51
hossainarif37 <hossainarif4937@gmail.com>
2022-05-17 18:44:21 +0600

    add item issue solved

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,10 +27,18 @@ async function run() {
             const result = await smartPhoneCollection.insertOne(products);
             res.send('inserted done')
         })
-        app.get('/myitems', async (req, res) => {
+        app.get('/allproducts', async (req, res) => {
             const cursor = smartPhoneCollection.find({});
             const result = await cursor.toArray();
             res.send(result);
+        })
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) }
+            const result = smartPhoneCollection.findOne(query);
+            res.send(result);
+            console.log(result);
         })
 
 
