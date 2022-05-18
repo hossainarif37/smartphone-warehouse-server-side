366139d788c619967092877ec1b8b60538d60616
hossainarif37 <hossainarif4937@gmail.com>
2022-05-19 03:28:10 +0600

    some code update

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// warehouse
-// RS8Bgp9SGIrWoJpC
+
 
 const uri = `mongodb+srv://${process?.env?.DB_USER}:${process?.env?.DB_PASS}@cluster0.oudjo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
